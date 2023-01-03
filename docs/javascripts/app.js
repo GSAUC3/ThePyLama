463225945e64a071301f537f0fb84eda929573b4
Rajarshi Banerjee <rajarshiban13@gmail.com>
2023-01-04 00:29:58 +0530

    almost ready

--- docs/javascripts/app.js
+++ docs/javascripts/app.js
@@ -13,19 +13,3 @@ function toggleButton() {
     console.log("called")
 }
 
-
-// // let bg = document.getElementById("bg")
-// let moon = document.getElementById("moon")
-// let mountain = document.getElementById("mountain")
-// let road = document.getElementById("road")
-
-// window.addEventListener('scroll',function(){
-//     var value = window.scrollY;
-
-//     // bg.style.top= value*0.5+'px';
-//     // moon.style.transform='translateY('+ value*(-0.5)+'px)';
-//     moon.style.left= -value*0.5+'px';
-//     mountain.style.top= -value*0.1+'px';
-//     road.style.top= value*0.3+'px';
-
-// })
